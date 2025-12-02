97757bf15ddad49dfbdc8f57e3ddf14c09d0d5b4
Mohit Jogiwala <149872549+Dev-Mohit-1220@users.noreply.github.com>
2025-12-02 12:22:43 +0530

    Initialize project

---
+++ README.md
@@ -0,0 +1,4 @@
+# Angular Short Examples
+
+### Command for the generate project like this.
+```npx @angular/cli@18 new angular-short-examples --no-standalone --directory=. --prefix=mike```
---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'mike-root',
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss'
+})
+export class AppComponent {
+  title = 'angular-short-examples';
+}
